a925a026218aeefaaa6bc66ee5b24e19f26a34e7
Filipe Amaral <filipe.santos.serra.amaral@gmail.com>
2018-10-13 15:12:45 +0100

    Adicionar arquivos de projeto.

---
+++ TutorialMVC_170221082/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TutorialMVC_170221082.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
