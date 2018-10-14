d500218bfb83f000a07e7b41079a73ff3b6c0420
Filipe Amaral <filipe.santos.serra.amaral@gmail.com>
2018-10-14 18:35:33 +0100

    Tutorial

---
+++ TutorialMVC_170221082/Data/TutorialMVC_170221082Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TutorialMVC_170221082.Models
+{
+    public class TutorialMVC_170221082Context : DbContext
+    {
+        public TutorialMVC_170221082Context (DbContextOptions<TutorialMVC_170221082Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TutorialMVC_170221082.Models.Movie> Movie { get; set; }
+    }
+}
--- TutorialMVC_170221082/Startup.cs
+++ TutorialMVC_170221082/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using TutorialMVC_170221082.Models;
 
 namespace TutorialMVC_170221082
 {
@@ -33,6 +35,9 @@ namespace TutorialMVC_170221082
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TutorialMVC_170221082Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TutorialMVC_170221082Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
